b384e8717a9861a1433a563f9f7812d6b55390c2
Marcin Dźwigała <marcin.dzwigala@docplanner.com>
2020-12-04 17:07:44 +0100

    json request bundle + readme

---
+++ README.md
@@ -0,0 +1,13 @@
+**Action-Domain-Responder implementation using Symfony Framework**
+
+`composer install`
+
+Endpoint:
+`/user/add [POST]`
+Params:
+```
+{
+    "email": "sample@foo.pl",
+    "password": "123456"
+}
+```
--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    SymfonyBundles\JsonRequestBundle\SymfonyBundlesJsonRequestBundle::class => ['all' => true],
 ];
