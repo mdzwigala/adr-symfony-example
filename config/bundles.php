4a9080c0b800b8ba8af0312dab8748a7dc28088d
Tac Tacelosky <tacman@gmail.com>
2022-06-19 07:45:38 -0400

    remove symfony-bundles/json-request-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,5 +3,4 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
 ];
