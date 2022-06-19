bf2b3933887dc6bd6227555bbe84a6c238a3b2b3
Tac Tacelosky <tacman@gmail.com>
2022-06-19 07:07:05 -0400

    update recipe

--- x.sh
+++ x.sh
@@ -1,2 +1,2 @@
-composer recipes:install --force symfony/$1 && bin/console cache:clear
+composer recipes:install --force doctrine/$1 && bin/console cache:clear
 #&& git add . && git commit -m "update recipe" && composer recipes
